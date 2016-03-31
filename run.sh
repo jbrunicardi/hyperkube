7addd150a4d2e65851b6bb8a7679bb073072f55e
jbrunicardi <jbrunicardi@gmail.com>
2016-03-31 17:50:32 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 set -e
 
 echo "Waiting for rpcbind to come up..."
-active=$(/usr/sbin/service rpcbind status | grep "[ok]" )
+active=$(/usr/sbin/service rpcbind status | grep "rpcbind is running" )
 while [ -z "$active" ]; do
   printf "."
   /usr/sbin/service rpcbind stop
   /usr/sbin/service rpcbind start
   sleep 5
-  active=$(/usr/sbin/service rpcbind status | grep "[ok]" )
+  active=$(/usr/sbin/service rpcbind status | grep "rpcbind is running" )
 done
 echo "rpcbind is up and running."
 
